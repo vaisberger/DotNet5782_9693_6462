1d7f4b97eb8915e519ecdb71b58e989db2acd328
vaisberger <yaelw123@gmail.com>
2021-10-09 21:18:07 +0300

    welcome function

---
+++ DotNet5782_9693_6462/Targil0/Program6462.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6462();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome9693();
+
+        private static void Welcome6462()
+        {
+            Console.WriteLine("Enter your name: ");
+            string ans = Console.ReadLine();
+            Console.WriteLine(", welcome to my first console application", ans);
+        }
+    }
+}
