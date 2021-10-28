d5cf0fc100823838e31836662142c96fd6c45a95
Moria123 <morimiz42@gmail.com>
2021-10-28 14:30:18 +0300

    b

--- DotNet5782_9693_6462/DAL/Parcel.cs
+++ DotNet5782_9693_6462/DAL/Parcel.cs
@@ -21,8 +21,12 @@ namespace IDAL
             public DateTime Scheduled { set; get; }
             public DateTime PickedUp { set; get; }
             public DateTime Delivered { set; get; }
-        }
 
+            public static void Tostring(int Id, int Senderld, int Targetld, Weights weight, Priorities priorty, DateTime Requsted, int Droneld, DateTime Scheduled, DateTime PickedUp, DateTime Delivered)
+            {
+                Console.WriteLine("ID: {0}   Senderld: {1} Longitude: {2} Targetld: {3}  weight: {4} priorty:{5}  Requsted:{6}  Droneld:{7} Scheduled:{8} PickedUp:{9} Delivered ", Id, Senderld, Targetld, weight, priorty, Requsted, Droneld, Scheduled, PickedUp, Delivered);
+            }
+        }
 
     }
 }
