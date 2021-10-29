4024a057ce6f9ac047430b23b49ba48933158c73
vaisberger <yaelw123@gmail.com>
2021-10-29 09:07:03 +0300

    1

--- DotNet5782_9693_6462/DAL/DataSource.cs
+++ DotNet5782_9693_6462/DAL/DataSource.cs
@@ -23,17 +23,26 @@ namespace DalObject
         internal static void Initialize()
         {
             Random r = new Random();
-            drones.Add(new Drone { Id = r.Next(1000000,10000000), Model = "x400", status = DroneStatus.Available , Battery = r.Next(5,100),weight=Weights.Heavy });
-            drones.Add(new Drone { Id = r.Next(1000000, 10000000), Model = "x401", status = DroneStatus.Maitenance, Battery = r.Next(5, 100), weight= Weights.Light });
-            drones.Add(new Drone { Id = r.Next(1000000, 10000000), Model = "x402", status = DroneStatus.Shipment, Battery = r.Next(5, 100), weight= Weights.Medium });
-            drones.Add(new Drone { Id = r.Next(1000000, 10000000), Model = "x403", status = DroneStatus.Available, Battery = r.Next(5, 100), weight= Weights.Heavy });
-            drones.Add(new Drone { Id = r.Next(1000000, 10000000), Model = "x404", status = DroneStatus.Maitenance, Battery = r.Next(5, 100), weight= Weights.Light });
+            drones.Add(new Drone { Id = 0, Model = "x400", status = DroneStatus.Available, Battery = r.Next(5, 100), weight = Weights.Heavy });
+            drones.Add(new Drone { Id = 0, Model = "x401", status = DroneStatus.Maitenance, Battery = r.Next(5, 100), weight = Weights.Light });
+            drones.Add(new Drone { Id = 0, Model = "x402", status = DroneStatus.Shipment, Battery = r.Next(5, 100), weight = Weights.Medium });
+            drones.Add(new Drone { Id = 0, Model = "x403", status = DroneStatus.Available, Battery = r.Next(5, 100), weight = Weights.Heavy });
+            drones.Add(new Drone { Id = 0, Model = "x404", status = DroneStatus.Maitenance, Battery = r.Next(5, 100), weight = Weights.Light });
 
-            station.Add(new BaseStation { Id = r.Next(1000,10000),Name=1212,Longitude=r.Next(10000,100000), Latitude = r.Next(10000, 100000),ChargeSlots=r.Next(0,6) });
+            station.Add(new BaseStation { Id = r.Next(1000, 10000), Name = 1212, Longitude = r.Next(10000, 100000), Latitude = r.Next(10000, 100000), ChargeSlots = r.Next(0, 6) });
             station.Add(new BaseStation { Id = r.Next(1000, 10000), Name = 1313, Longitude = r.Next(10000, 100000), Latitude = r.Next(10000, 100000), ChargeSlots = r.Next(0, 6) });
 
-            parcels.Add(new Parcel {Id=r.Next(100,10000),Senderld=$"1{r.Next()}" });
-        }
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+            parcels.Add(new Parcel { Id = r.Next(100, 10000), Senderld = 0, Targetld = 0, weight = Weights.Light, priorty = Priorities.Normal, Requsted = DateTime.Now, Droneld = 0, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now });
+        }   
     }
 
 }
