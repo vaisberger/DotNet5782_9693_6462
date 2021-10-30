8702d6dea1b04ddd99898d8b5b647a72923b28e5
Moria123 <morimiz42@gmail.com>
2021-10-30 23:04:44 +0300

    1

--- DotNet5782_9693_6462/DAL/DalObjects.cs
+++ DotNet5782_9693_6462/DAL/DalObjects.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using IDAL.DO;
 
 
 namespace DalObject
 {
     public class DalObject
     {
-        
-
-
-
+        public static void AddCustomer(Customer c)
+        {
+            DataSource.customers.Add(c);
+        }
+        public static void AddStation(BaseStation s)
+        {
+            DataSource.station.Add(s);
+        }
+        public static void AddDrone(Drone d)
+        {
+            DataSource.drones.Add(d);
+        }
+        public static void AddParcel(Parcel p)
+        {
+            DataSource.parcels.Add(p);
+        }
         public static void DisplayStation(int Id)
         {
             DataSource.station.Find(x => x.Id == Id).ToString();
@@ -26,6 +39,60 @@ namespace DalObject
             DataSource.parcels.Find(x => x.Id == Id).ToString();
 
         }
+        public static void MatchDrone(Parcel p)
+        {
+
+        }
+        public static void DisplayDroneList()
+        {
+            foreach(Drone d in DataSource.drones)
+            {
+                Console.WriteLine(d.ToString());
+            }
+        }
+        public static void DisplayStationList()
+        {
+            foreach (BaseStation b in DataSource.station)
+            {
+                Console.WriteLine(b.ToString());
+            }
+        }
+        public static void DisplayCustomerList()
+        {
+            foreach (Customer c in DataSource.customers)
+            {
+                Console.WriteLine(c.ToString());
+            }
+           
+        }
+        public static void DisplayParcelList()
+        {
+            foreach (Parcel p in DataSource.parcels)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+        public static void DisplayDroneNot()
+        {
+            foreach(Drone d in DataSource.drones)
+            {
+                if (d.status==0)
+                {
+                    Console.WriteLine(d.ToString());
+                }
+            }
+        }
+        public static void DisplayAvailableStation()
+        {
+           foreach (BaseStation b in DataSource.station)
+            {
+                if( b.Longitude!=0)
+                {
+                    Console.WriteLine(b.ToString());
+                }
+            }
+        }
+
 
     }
 }
