65de0bf6893c0ed51e8f11b2bab3ed5913385260
vaisberger <yaelw123@gmail.com>
2021-10-30 23:04:50 +0300

    4

--- DotNet5782_9693_6462/DAL/DalObjects.cs
+++ DotNet5782_9693_6462/DAL/DalObjects.cs
@@ -6,11 +6,6 @@ namespace DalObject
 {
     public class DalObject
     {
-        
-
-
-
-        public static void DisplayStation(int Id)
         public static void AddCustomer(Customer c)
         {
             DataSource.customers.Add(c);
@@ -29,7 +24,43 @@ namespace DalObject
         }
 
 
+        public static void DisplayStation(int Id)
+        {
+            DataSource.station.Find(x => x.Id == Id).ToString();
+        }
+        public static void DispalyDrone(int Id)
+        {
+            DataSource.drones.Find(x => x.Id == Id).ToString();
+        }
+        public static void DisplayCostomer(int Id)
+        {
+            DataSource.customers.Find(x => x.Id == Id).ToString();
+        }
+        public static void DisplayParcel(int Id)
+        {
+            DataSource.parcels.Find(x => x.Id == Id).ToString();
+
+        }
+
         public static void MatchDrone(Parcel p)
+        {
+            DataSource.drones.Find(x => x.Id == p.Droneld);
+            
+        }
+
+        public static void ParcelCollection(Drone d)
+        {
+
+        }
+        public static void ParcelDelivery(Parcel p)
+        {
+
+        }
+        public static void ChargeDrone(Drone d)
+        {
+
+        }
+        public static void DischargeDrone(Drone d)
         {
 
         }
