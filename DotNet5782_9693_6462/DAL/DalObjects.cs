0366a476946d3706adb5be57f707590bb23a9551
vaisberger <yaelw123@gmail.com>
2021-10-30 20:36:34 +0300

    1

--- DotNet5782_9693_6462/DAL/DalObjects.cs
+++ DotNet5782_9693_6462/DAL/DalObjects.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using IDAL.DO;
+using System;
 
 namespace DalObject
 {
     public class DalObject
     {
-       
+        public static void AddCustomer(Customer c)
+        {
+            DataSource.customers.Add(c);
+        }
+        public static void AddStation(BaseStation s)
+        {
+            DataSource.station.Add(s);
+        }
+        public static void AddDrone(Drone d)
+        {
+            DataSource.drones.Add(d);
+        }
+        public static void AddParcel(Parcel p)
+        {
+            DataSource.parcels.Add(p);
+        }
+
+
+        public static void MatchDrone(Parcel p)
+        {
+
+        }
     }
 }
