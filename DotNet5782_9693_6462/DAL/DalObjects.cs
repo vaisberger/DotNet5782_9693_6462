8900f6e11636d39b167b628f3f99dd51f24e3c5c
Moria123 <morimiz42@gmail.com>
2021-10-30 20:35:41 +0300

    1

--- DotNet5782_9693_6462/DAL/DalObjects.cs
+++ DotNet5782_9693_6462/DAL/DalObjects.cs
@@ -1,9 +1,38 @@
 ﻿using System;
 
+
 namespace DalObject
 {
     public class DalObject
     {
-       
+        public static void AddCustomer(IDAL.DO.Customer c)
+        {
+            DataSource.customers.Add(c);
+        }
+        public static void AddStation(IDAL.DO.BaseStation b)
+        {
+            DataSource.station.Add(b);
+        }
+
+
+
+        public static void DisplayStation(int Id)
+        {
+            DataSource.station.Find(x => x.Id == Id).ToString();
+        }
+        public static void DispalyDrone(int Id)
+        {
+            DataSource.drones.Find(x => x.Id == Id).ToString();
+        }
+        public static void DisplayCostomer(int Id)
+        {
+            DataSource.customers.Find(x => x.Id == Id).ToString();
+        }
+        public static void DisplayParcel(int Id)
+        {
+            DataSource.parcels.Find(x => x.Id == Id).ToString();
+
+        }
+
     }
 }
