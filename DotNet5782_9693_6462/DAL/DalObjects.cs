16c5bee58d4aa3309db1f1227706aacf618d8222
Moria123 <morimiz42@gmail.com>
2021-11-18 12:14:37 +0200

    455

--- DotNet5782_9693_6462/DAL/DalObjects.cs
+++ DotNet5782_9693_6462/DAL/DalObjects.cs
@@ -4,262 +4,264 @@ using IDAL.DO;
 
 namespace DalObject
 {
+ 
 
-    public class DalObject
-    {
-        //constructor
-        public DalObject()
+        public class DalObject 
         {
-            DataSource.Initialize();
-        }
-
-        //Add a parcel  to the list of  parcel
-        public int AddParcel(Parcel p)
-        {
-            DataSource.parcels.Add(p);
-            DataSource.Config.ParcelSerial += 1;
-            return DataSource.Config.ParcelSerial;
-        }
-        //Add customer to the list of customer
-        public void AddCustomer(Customer c)
-        {
-            DataSource.customers.Add(c);
-        }
-
-        //Add a base station to the list of stations
-        public void AddStation(ref BaseStation s)
-        {
-            DataSource.stations.Add(s);
-        }
-        // Add a drone  to the list of  drones
-        public void AddDrone(Drone d)
-        {
-            DataSource.drones.Add(d);
-        }
-        //Update parcel  to a drone
-        public void UpdateParcelToDrone(int droneId, int parcleId)
-        {
-            for (int i = 0; i < DataSource.parcels.Count; i++)
+            //constructor
+            public DalObject()
             {
-                if (DataSource.parcels[i].Id == parcleId)
-                {
-                    Parcel P = new Parcel();
-                    P = DataSource.parcels[i];
-                    P.DroneId = droneId;
-                    P.Scheduled = DateTime.Now;
-                    DataSource.parcels[i] = P;
-
-
-                }
+                DataSource.Initialize();
             }
-        }
 
-        //Parcel collection drone
-        public void Parcelcollection(int p, int d)
-        {
-            for (int i = 0; i < DataSource.parcels.Count; i++)
+            //Add a parcel  to the list of  parcel
+            public int AddParcel(Parcel p)
             {
-                if (DataSource.parcels[i].Id == p)
-                {
-                    //d.status = DroneStatus.Shipment;
-                    Parcel P = new Parcel();
-                    P = DataSource.parcels[i];
-                    P.DroneId = d;
-                    P.PickedUp = DateTime.Now;
-                    DataSource.parcels[i] = P;
-
-
-                }
+                DataSource.parcels.Add(p);
+                DataSource.Config.ParcelSerial += 1;
+                return DataSource.Config.ParcelSerial;
             }
-            for (int i = 0; i < DataSource.drones.Count; i++)
+            //Add customer to the list of customer
+            public void AddCustomer(Customer c)
             {
-                if (DataSource.drones[i].Id == d)
+                DataSource.customers.Add(c);
+            }
+
+            //Add a base station to the list of stations
+            public void AddStation(ref BaseStation s)
+            {
+                DataSource.stations.Add(s);
+            }
+            // Add a drone  to the list of  drones
+            public void AddDrone(Drone d)
+            {
+                DataSource.drones.Add(d);
+            }
+            //Update parcel  to a drone
+            public void UpdateParcelToDrone(int droneId, int parcleId)
+            {
+                for (int i = 0; i < DataSource.parcels.Count; i++)
                 {
-                    Drone D = new Drone();
-                    D = DataSource.drones[i];
-                    //D.status = DroneStatus.Shipment;
-                    DataSource.drones[i] = D;
+                    if (DataSource.parcels[i].Id == parcleId)
+                    {
+                        Parcel P = new Parcel();
+                        P = DataSource.parcels[i];
+                        P.DroneId = droneId;
+                        P.Scheduled = DateTime.Now;
+                        DataSource.parcels[i] = P;
+
+
+                    }
                 }
             }
 
+            //Parcel collection drone
+            public void Parcelcollection(int p, int d)
+            {
+                for (int i = 0; i < DataSource.parcels.Count; i++)
+                {
+                    if (DataSource.parcels[i].Id == p)
+                    {
+                        //d.status = DroneStatus.Shipment;
+                        Parcel P = new Parcel();
+                        P = DataSource.parcels[i];
+                        P.DroneId = d;
+                        P.PickedUp = DateTime.Now;
+                        DataSource.parcels[i] = P;
 
-        }
-        //Delivery of a parcel to the customer
-        public void ParcelDelivery(int p, int c)
-        {
-            for (int i = 0; i < DataSource.parcels.Count; i++)
-            {
-                if (DataSource.parcels[i].Id == p)
-                {
-                    //d.status = DroneStatus.Shipment;
-                    Parcel P = new Parcel();
-                    P = DataSource.parcels[i];
-                    P.SenderId = c;
-                    P.TargetId = c;
-                    P.Delivered = DateTime.Now;
-                    DataSource.parcels[i] = P;
 
+                    }
                 }
-            }
-        }
-        //Sending drone for charging at a base station
-        public void ChargeDrone(int d, int s)
-        {
-            for (int i = 0; i < DataSource.drones.Count; i++)
-            {
-                if (DataSource.drones[i].Id == d)
+                for (int i = 0; i < DataSource.drones.Count; i++)
                 {
-                    Drone D = new Drone();
-                    D = DataSource.drones[i];
-                   // D.status = DroneStatus.Maitenance;
-                    DataSource.drones[i] = D;
+                    if (DataSource.drones[i].Id == d)
+                    {
+                        Drone D = new Drone();
+                        D = DataSource.drones[i];
+                        //D.status = DroneStatus.Shipment;
+                        DataSource.drones[i] = D;
+                    }
                 }
-            }
-            for (int i = 0; i < DataSource.stations.Count; i++)
-            {
-                if (DataSource.stations[i].Id == s)
-                {
-                    BaseStation S = new BaseStation();
-                    S = DataSource.stations[i];
-                    S.ChargeSlots -= 1;
-                    DataSource.stations[i] = S;
-                }
-            }
-            DroneCharge dc = new DroneCharge();
-            dc.DroneId = d;
-            dc.StatioId = s;
-            DataSource.droneCharges.Add(dc);
 
-        }
-        //Discharge drone from charging at base station
-        public void DischargeDrone(int d)
-        {
-            DroneCharge dc;
-            for (int i = 0; i < DataSource.drones.Count; i++)
-            {
-                if (DataSource.drones[i].Id == d)
-                {
-                    Drone D = new Drone();
-                    D = DataSource.drones[i];
-                    //D.status = DroneStatus.Available;
-                    DataSource.drones[i] = D;
-                }
-            }
-            for (int i = 0; i < DataSource.droneCharges.Count; i++)
-            {
-                if (DataSource.droneCharges[i].DroneId == d)
-                {
-                    dc = DataSource.droneCharges[i];
-                    DataSource.droneCharges.Remove(dc);
-                }
-            }
 
-        }
+            }
+            //Delivery of a parcel to the customer
+            public void ParcelDelivery(int p, int c)
+            {
+                for (int i = 0; i < DataSource.parcels.Count; i++)
+                {
+                    if (DataSource.parcels[i].Id == p)
+                    {
+                        //d.status = DroneStatus.Shipment;
+                        Parcel P = new Parcel();
+                        P = DataSource.parcels[i];
+                        P.SenderId = c;
+                        P.TargetId = c;
+                        P.Delivered = DateTime.Now;
+                        DataSource.parcels[i] = P;
 
-        ////Drone display by ID
-        public void DisplayStation(int Id)
-        {
-            for(int i=0; i<DataSource.stations.Count; i++)
-            {
-                if(DataSource.stations[i].Id==Id)
-                {
-                    Console.WriteLine(DataSource.stations[i].ToString());
-                    return;
+                    }
                 }
             }
-        }
-        public void DisplayDrone(int Id)
-        {
-            for (int i = 0; i < DataSource.drones.Count; i++)
+            //Sending drone for charging at a base station
+            public void ChargeDrone(int d, int s)
             {
-                if (DataSource.drones[i].Id == Id)
+                for (int i = 0; i < DataSource.drones.Count; i++)
                 {
-                    Console.WriteLine(DataSource.drones[i].ToString());
+                    if (DataSource.drones[i].Id == d)
+                    {
+                        Drone D = new Drone();
+                        D = DataSource.drones[i];
+                        // D.status = DroneStatus.Maitenance;
+                        DataSource.drones[i] = D;
+                    }
+                }
+                for (int i = 0; i < DataSource.stations.Count; i++)
+                {
+                    if (DataSource.stations[i].Id == s)
+                    {
+                        BaseStation S = new BaseStation();
+                        S = DataSource.stations[i];
+                        S.ChargeSlots -= 1;
+                        DataSource.stations[i] = S;
+                    }
+                }
+                DroneCharge dc = new DroneCharge();
+                dc.DroneId = d;
+                dc.StatioId = s;
+                DataSource.droneCharges.Add(dc);
 
-                    return;
-                }
             }
-        }
-  
-        //Displays customer by ID 
-        public void DisplayCustomer(int Id)
-        {
-            for(int i=0; i<DataSource.customers.Count; i++)
+            //Discharge drone from charging at base station
+            public void DischargeDrone(int d)
             {
-                if (DataSource.customers[i].Id==Id)
+                DroneCharge dc;
+                for (int i = 0; i < DataSource.drones.Count; i++)
                 {
-                    Console.WriteLine(DataSource.customers[i].ToString());
-                    return;
+                    if (DataSource.drones[i].Id == d)
+                    {
+                        Drone D = new Drone();
+                        D = DataSource.drones[i];
+                        //D.status = DroneStatus.Available;
+                        DataSource.drones[i] = D;
+                    }
                 }
-            }
-        }
-        //Displays parcel by ID 
-        public void DisplayParcel(int Id)
-        {
-            for(int i=0; i<DataSource.parcels.Count;i++)
-            {
-                if(DataSource.parcels[i].Id==Id)
+                for (int i = 0; i < DataSource.droneCharges.Count; i++)
                 {
-                    Console.WriteLine(DataSource.parcels[i].ToString());
-                    return;
+                    if (DataSource.droneCharges[i].DroneId == d)
+                    {
+                        dc = DataSource.droneCharges[i];
+                        DataSource.droneCharges.Remove(dc);
+                    }
                 }
-            }
-        }
-        //Displays a list of drone
-        public void DisplayDroneList()
-        {
-            foreach (Drone d in DataSource.drones)
-            {
-                Console.WriteLine(d.ToString());
-            }
-        }
-        //Displays a list of base stations
-        public void DisplayStationList()
-        {
-            foreach (BaseStation b in DataSource.stations)
-            {
-                Console.WriteLine(b.ToString());
-            }
-        }
-        //Displays a list of customer
-        public void DisplayCustomerList()
-        {
-            foreach (Customer c in DataSource.customers)
-            {
-                Console.WriteLine(c.ToString());
+
             }
 
-        }
-        //Displays a list of parcel
-        public void DisplayParcelList()
-        {
-            foreach (Parcel p in DataSource.parcels)
+            ////Drone display by ID
+            public void DisplayStation(int Id)
             {
-                Console.WriteLine(p.ToString());
-            }
-        }
-        //Displays a list of parcel not yet associated with the drone
-        public void DisplayParcelUnmatched()
-        {
-            foreach (Parcel p in DataSource.parcels)
-            {
-                if (p.DroneId == 0)
+                for (int i = 0; i < DataSource.stations.Count; i++)
                 {
-                    Console.WriteLine(p.ToString());
+                    if (DataSource.stations[i].Id == Id)
+                    {
+                        Console.WriteLine(DataSource.stations[i].ToString());
+                        return;
+                    }
                 }
             }
-        }
-        //Displays base stations with available charging stations
-        public void DisplayAvailableStation()
-        {
-            foreach (BaseStation b in DataSource.stations)
+            public void DisplayDrone(int Id)
             {
-                if (b.Longitude != 0)
+                for (int i = 0; i < DataSource.drones.Count; i++)
+                {
+                    if (DataSource.drones[i].Id == Id)
+                    {
+                        Console.WriteLine(DataSource.drones[i].ToString());
+
+                        return;
+                    }
+                }
+            }
+
+            //Displays customer by ID 
+            public void DisplayCustomer(int Id)
+            {
+                for (int i = 0; i < DataSource.customers.Count; i++)
+                {
+                    if (DataSource.customers[i].Id == Id)
+                    {
+                        Console.WriteLine(DataSource.customers[i].ToString());
+                        return;
+                    }
+                }
+            }
+            //Displays parcel by ID 
+            public void DisplayParcel(int Id)
+            {
+                for (int i = 0; i < DataSource.parcels.Count; i++)
+                {
+                    if (DataSource.parcels[i].Id == Id)
+                    {
+                        Console.WriteLine(DataSource.parcels[i].ToString());
+                        return;
+                    }
+                }
+            }
+            //Displays a list of drone
+            public void DisplayDroneList()
+            {
+                foreach (Drone d in DataSource.drones)
+                {
+                    Console.WriteLine(d.ToString());
+                }
+            }
+            //Displays a list of base stations
+            public void DisplayStationList()
+            {
+                foreach (BaseStation b in DataSource.stations)
                 {
                     Console.WriteLine(b.ToString());
                 }
             }
-        }
+            //Displays a list of customer
+            public void DisplayCustomerList()
+            {
+                foreach (Customer c in DataSource.customers)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+
+            }
+            //Displays a list of parcel
+            public void DisplayParcelList()
+            {
+                foreach (Parcel p in DataSource.parcels)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+            //Displays a list of parcel not yet associated with the drone
+            public void DisplayParcelUnmatched()
+            {
+                foreach (Parcel p in DataSource.parcels)
+                {
+                    if (p.DroneId == 0)
+                    {
+                        Console.WriteLine(p.ToString());
+                    }
+                }
+            }
+            //Displays base stations with available charging stations
+            public void DisplayAvailableStation()
+            {
+                foreach (BaseStation b in DataSource.stations)
+                {
+                    if (b.Longitude != 0)
+                    {
+                        Console.WriteLine(b.ToString());
+                    }
+                }
+            }
+        
 
 
 
---
+++ DotNet5782_9693_6462/DAL/IDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+
+
+namespace DAL
+{
+    namespace DalObject
+    {
+         interface IDal
+        {
+            public int AddParcel(Parcel p);
+      
+        }
+    }
+}
