45c46bfb07428a72d6a4fcaf17f747e823591da0
Moria123 <morimiz42@gmail.com>
2021-12-04 23:26:53 +0200

    56

--- DotNet5782_9693_6462/PL/ListDrone.xaml.cs
+++ DotNet5782_9693_6462/PL/ListDrone.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ListDrone.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ListDrone : Window
     {
-        public MainWindow()
+        public ListDrone()
         {
             InitializeComponent();
         }
