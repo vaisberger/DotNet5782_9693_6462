a99f4ce34039312bd82d5f2b68a989b57a0412bc
vaisberger <yaelw123@gmail.com>
2021-12-05 16:01:13 +0200

    7

--- DotNet5782_9693_6462/PL/ListDroneWindow.xaml.cs
+++ DotNet5782_9693_6462/PL/ListDroneWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,22 +14,24 @@ using System.Windows.Shapes;
 using IBL.BO;
 using BL;
 
-
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for ListDrone.xaml
+    /// Interaction logic for ListDroneWindow.xaml
     /// </summary>
-
     public partial class ListDroneWindow : Window
     {
-        IBL.IBL bl;
-        public ListDroneWindow(IBL.IBL THBL)
+
+        public ListDroneWindow()
         {
-            this.bl = THBL;
-            
-            
+          
+            InitializeComponent();
+            cmbStatus.ItemsSource = Enum.GetValues(typeof(IBL.BO.DroneStatus));
         }
 
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
