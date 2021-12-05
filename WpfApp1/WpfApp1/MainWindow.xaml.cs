18165d93abcf1c742bb1a65e8a75018303194b95
Moria123 <morimiz42@gmail.com>
2021-12-05 17:36:30 +0200

    89

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -10,33 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using IBL;
-namespace PL
+
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-       // IBL.BO.IBL mybl;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-       
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Button clicked!");
-            //new ListDroneWindow(mybl).Show();
-            
         }
-
-        /*private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            new Drone(mybl).Show();
-        }*/
     }
 }
