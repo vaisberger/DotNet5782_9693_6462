01514dab8116ae868edb1d113345a93915977764
vaisberger <yaelw123@gmail.com>
2021-12-09 19:00:22 +0200

    1

---
+++ DotNet5782_9693_6462/DAL/DalFactory.cs
@@ -0,0 +1,18 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public static class DalFactory
+    {
+        public static IDal GetDAL(string str)
+        {
+            return IDal;
+        }
+
+    }
+}
