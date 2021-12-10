efa69004c25b8d9d8df074b7203d5a5d17295bf8
Moria123 <morimiz42@gmail.com>
2021-12-10 11:01:25 +0200

    78

---
+++ DotNet5782_9693_6462/BL/BlFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+namespace BL
+{
+    public static class BlFactory
+    {
+
+        public static IBl GetBl()
+        {
+            BO.IBl ibl;
+
+            return ibl;
+        }
+    }
+}
