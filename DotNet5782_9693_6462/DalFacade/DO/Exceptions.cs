3487a0075fa5c9e866c8e4aea7bd6dcf0a2437a1
Moria123 <morimiz42@gmail.com>
2022-01-02 23:01:42 +0200

    677

---
+++ DotNet5782_9693_6462/DalFacade/DO/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    [Serializable]
+    public class IDExistsInTheSystem:Exception
+    {
+        public int ID;
+        public IDExistsInTheSystem(int id) : base() => ID = id;
+        public IDExistsInTheSystem(int id, string message) : base(message) => ID = id;
+        public IDExistsInTheSystem(int id, string message, Exception exception) : base(message, exception) => ID = id;
+        public override string ToString()
+        {
+            return base.ToString() + $",Exits id:{ID}";
+        }
+
+
+
+    }
+    [Serializable]
+    public class IDNotExistsInTheSystem:Exception
+    {
+        public int ID;
+        public IDNotExistsInTheSystem(int id) : base() => ID = id;
+        public IDNotExistsInTheSystem(int id, string message) : base(message) => ID = id;
+        public IDNotExistsInTheSystem(int id, string message, Exception exception) => ID = id;
+        public override string ToString()
+        {
+            return base.ToString() + $", Doesn't exits id:{ID}";
+        }
+    }
+}
